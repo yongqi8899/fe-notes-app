3ef68f781438b1c8d38e2288a6cb6a38177e3cd9
yongqi8899 <1243994415@qq.com>
2024-07-12 12:52:44 +0200

    add note in progress

--- src/components/Notes/NoteCard.jsx
+++ src/components/Notes/NoteCard.jsx
@@ -4,9 +4,12 @@ import { toast } from "react-toastify";
 const NoteCard = ({ note }) => {
   const [error, setError] = useState(null);
   const [aiImageUrl, setAiImageUrl] = useState("");
+  const [aiNote, setAiNote] = useState("");
   const [loadingAiImg, setloadingAiImg] = useState(false);
+  const [loadingAiNote, setloadingAiNote] = useState(false);
 
   const src = aiImageUrl ? aiImageUrl : note.image;
+  const aiNoteSrc = aiNote ? aiNote : note.content;
   const fetchAIImage = async (e) => {
     e.preventDefault();
     try {
@@ -34,10 +37,10 @@ const NoteCard = ({ note }) => {
       const imageUrl = data[0].url;
       setAiImageUrl(imageUrl);
       editImage(imageUrl);
-      toast.success(`AI image is ready`);
+      toast.success(`AI node is ready`);
     } catch (error) {
       setError(error);
-      toast.error(`Failed to fetch AI image`);
+      toast.error(`Failed to fetch AI node`);
       console.log(error);
     } finally {
       setloadingAiImg(false);
@@ -62,10 +65,49 @@ const NoteCard = ({ note }) => {
     console.log("edit", data);
   };
 
-  const fetchAINote = (e) => {
+  const fetchAINote = async (e) => {
     e.preventDefault();
-    
-  }
+    setloadingAiNote(true);
+    try {
+      const response = await fetch(
+        "https://gen-ai-wbs-consumer-api.onrender.com/api/v1/chat/completions",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            mode: "production",
+            provider: "open-ai",
+            Authorization: "g1tktjt6lxrck90us9748q",
+          },
+          body: JSON.stringify({
+            model: "gpt-4o",
+            // stream: streamValue,
+            messages: [
+              {
+                role: "system",
+                content:
+                  "You are a software developer student that only speaks in JS, HTML, CSS, Typscript, React and so on. You are building a chatbot that can help users with their notes. The user will provide a prompt and the chatbot will generate a note based on the prompt. The user can also ask the chatbot to read the note. The chatbot can also generate an image based on the note. The chatbot can also generate a summary of the note. The chatbot can also generate a question based on the note.",
+              },
+              {
+                role: "user",
+                content: `write a note about${note.title}, it schould be include intruduction ${note.title}, ${note.content} and example about ${note.title}. `,
+              },
+            ],
+          }),
+        }
+      );
+      const data = await response.json();
+      let aiNode = data.message.content;
+      setAiNote(aiNode);
+      console.log("aiNode", aiNode);
+    } catch (error) {
+      setError(error);
+      toast.error(`Failed to fetch AI image`);
+      console.log(error);
+    } finally {
+      setloadingAiNote(false);
+    }
+  };
   return (
     <div className="shadow-xl card bg-base-100">
       <figure className="h-48 bg-white">
@@ -80,11 +122,19 @@ const NoteCard = ({ note }) => {
         <p className="truncate text-wrap">{note.content}</p>
       </div>
       <div className="flex justify-between">
-        <button className="text-white bg-purple-500 btn-sm btn hover:bg-purple-400" onClick={fetchAIImage} disabled={loadingAiImg}>
-        {loadingAiImg ? "loading..." : "AI Image"}
+        <button
+          className="text-white bg-purple-500 btn-sm btn hover:bg-purple-400"
+          onClick={fetchAIImage}
+          disabled={loadingAiImg}
+        >
+          {loadingAiImg ? "loading..." : "AI Image"}
         </button>
-        <button className="text-white bg-purple-500 btn-sm btn hover:bg-purple-400">
-          AI Note
+        <button
+          className="text-white bg-purple-500 btn-sm btn hover:bg-purple-400"
+          onClick={fetchAINote}
+          disabled={loadingAiNote}
+        >
+          {loadingAiNote ? "loading..." : "AI Note"}
         </button>
         <button className="text-white bg-purple-500 btn-sm btn hover:bg-purple-400">
           AI Read
